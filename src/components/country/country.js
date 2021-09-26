eb9a3a870587df358cb61d1fdaaef3ffe33300ad
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2021-09-26 12:33:57 +0300

    fixing flags

--- src/components/country/country.js
+++ src/components/country/country.js
@@ -5,9 +5,8 @@ class Country extends PureComponent{
         const {flag,name,subregion,region,capital,colorMode,onCountryClick,alpha2Code,cca3}=this.props;
         return(
             <div className={"Country "+colorMode+"-Country"}  onClick={onCountryClick.bind(this,alpha2Code,"out")}>
-                {/* <img src={flag} alt={name}/> */}
+                <img src={flag} alt={name}/>
                 {/* <img src={`https://restcountries.com/data/png/${cca3.toLowerCase()}.png`} alt={name}/> */}
-                <img src={`https://restcountries.com/data/${cca3.toLowerCase()}.svg`} alt={name}/>
                 <div className="infoContainer">
                   <p className="countryName">{name}</p>
                   <p className="countryInfo">{"Subregion:"+subregion}</p>
